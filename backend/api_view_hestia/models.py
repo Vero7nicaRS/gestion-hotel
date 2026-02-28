a83ec5dee240c0716d365d14ed4e7896aab52cef
Verónica <vero7nicaxs@gmail.com>
2026-02-28 20:23:03 +0100

    Actualización modelos: descripción opcional en "TipoSala" y "TipoHabitación"

--- backend/api_view_hestia/models.py
+++ backend/api_view_hestia/models.py
@@ -14,7 +14,7 @@ from django.db import models
 class TipoHabitacion(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    descripcion = models.CharField(max_length=250)
+    descripcion = models.CharField(max_length=250, null=True, blank=True) # Descripción opcional
     
     def __str__(self):
         return f"{self.nombre} - ${self.precio}"
@@ -40,7 +40,7 @@ class Habitacion(models.Model):
 class TipoSala(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    descripcion = models.TextField(max_length=250)
+    descripcion = models.CharField(max_length=250, null=True, blank=True) # Descripción opcional
     
     def __str__(self):
         return f"{self.nombre} - ${self.precio}"
