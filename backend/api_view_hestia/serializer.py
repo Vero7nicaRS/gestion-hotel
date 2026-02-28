dc23b11a24624314c291a6e363ed57d3d09cb765
Verónica <vero7nicaxs@gmail.com>
2026-02-28 16:11:59 +0100

    Agregar vistas y serializars: "TipoHabitacionViewSet", "TipoHabitacionSerializer", "HabitacionViewSet" y "HabitacionSerializer".

--- backend/api_view_hestia/serializer.py
+++ backend/api_view_hestia/serializer.py
@@ -1,35 +1,60 @@
 from rest_framework import serializers
-from .models import tipohabitacion, habitacion, tiposala, sala, cliente, reserva, reserva_sala, reserva_habitacion
+from .models import Habitacion, TipoHabitacion, TipoSala, Sala, Cliente, Reserva, ReservaSala, ReservaHabitacion
 
-class tipohabitacionSerializer(serializers.ModelSerializer):
+
+#                                   VALIDACIONES
+# SERIALIZER: Se encarga de validar que los datos que se pasado por el JSON (body) ---> Postman
+#             sean los correctos. Para que así, VIEW solamente tenga que implementar la funcionalidad
+#             y no hacer comprobaciones.
+#             Por tanto, el serializer se encarga de comprobar los datos de entrada (JSON - BODY)
+# validate_<NOMBRE_DEL_CAMPO_A_VALIDAR>
+# 
+# Serializer: permite definir que información de nuestro modelo vamos a mover hacia delante o hacia atrás
+# cuando el usuario interactue con nosotros. Y que limitaciones estamos imponiendo y validaciones adicionales.
+# Después del serializer, hay que dirigirse a la vista.
+
+class TipoHabitacionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = tipohabitacion
+        model = TipoHabitacion
         fields = ['id', 'nombre', 'precio', 'descripcion']
         read_only_fields = ['id']
 
-class habitacionSerializer(serializers.ModelSerializer):
-    idtipo_habitacion = tipohabitacionSerializer(read_only=True)
+
+# Lo que se recibe del JSON (body):
+# {
+#  "id": 1,
+#  "numero": "253",
+#  "estado": "DISPONIBLE",
+#  "tipo_habitacion": {
+#    "id": 3,
+#    "nombre": "Doble",
+#    "precio": "90.00",
+#    "descripcion": "Habitación con literas."
+#  }
+#}
+class HabitacionSerializer(serializers.ModelSerializer):
+    tipo_habitacion = TipoHabitacionSerializer(read_only=True)
     class Meta:
-        model = habitacion
-        fields = ['id', 'numero', 'idtipo_habitacion', 'estado']
+        model = Habitacion
+        fields = ['id', 'numero', 'tipo_habitacion', 'estado']
         read_only_fields = ['id']
 
-class tiposalaSerializer(serializers.ModelSerializer):
+class TipoSalaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = tiposala
+        model = TipoSala
         fields = ['id', 'nombre', 'precio', 'descripcion']
         read_only_fields = ['id']
 
-class salaSerializer(serializers.ModelSerializer):
-    idtipo_sala = tiposalaSerializer(read_only=True)
+class SalaSerializer(serializers.ModelSerializer):
+    idtipo_sala = TipoSalaSerializer(read_only=True)
     class Meta:
-        model = sala
+        model = Sala
         fields = ['id', 'numero', 'idtipo_sala', 'estado']
         read_only_fields = ['id']
 
 class clienteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = cliente
+        model = Cliente
         fields = ['id', 'nombre', 'email']
         read_only_fields = ['id']
 
@@ -38,22 +63,22 @@ class reservaSerializer(serializers.ModelSerializer):
     estado = serializers.CharField(source='get_estado_display', read_only=True)
     tipo_reserva = serializers.CharField(source='get_tipo_reserva_display', read_only=True)
     class Meta:
-        model = reserva
+        model = Reserva
         fields = ['id', 'idcliente','estado','tipo_reserva','fecha_reserva']
         read_only_fields = ['id']
 
 class reserva_salaSerializer(serializers.ModelSerializer):
     idreserva = reservaSerializer(read_only=True)
-    idsala = salaSerializer(read_only=True)
+    idsala = SalaSerializer(read_only=True)
     class Meta:
-        model = reserva_sala
+        model = ReservaSala
         fields = ['id', 'idreserva', 'idsala','numero_personas','fecha_uso', 'hora_inicio', 'hora_fin']
         read_only_fields = ['id']
 
 class reserva_habitacionSerializer(serializers.ModelSerializer):
     idreserva = reservaSerializer(read_only=True)
-    idhabitacion = habitacionSerializer(read_only=True)
+    idhabitacion = HabitacionSerializer(read_only=True)
     class Meta:
-        model = reserva_habitacion
+        model = ReservaHabitacion
         fields = ['id', 'idreserva', 'idhabitacion','numero_personas','fecha_entrada', 'fecha_salida']
         read_only_fields = ['id']
--- backend/api_view_hestia/views.py
+++ backend/api_view_hestia/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Habitacion, TipoHabitacion, TipoSala, Sala, Cliente, Reserva, ReservaSala, ReservaHabitacion
+from .serializer import HabitacionSerializer, TipoHabitacionSerializer, TipoSalaSerializer, SalaSerializer, clienteSerializer, reservaSerializer, reserva_salaSerializer, reserva_habitacionSerializer
+
+from rest_framework import viewsets
 
 # Create your views here.
+# VIEWSETS
+# ---------------------------------------------- HABITACION ---------------------------------------------
+class TipoHabitacionViewSet(viewsets.ModelViewSet):
+    queryset = TipoHabitacion.objects.all().order_by('nombre') # Obtener la informacion
+    serializer_class = TipoHabitacionSerializer   
+    lookup_field = 'pk'
+
+
+class HabitacionViewSet(viewsets.ModelViewSet):
+    queryset = Habitacion.objects.all().order_by('numero') # Obtener la informacion
+    serializer_class = HabitacionSerializer
+    lookup_field = 'pk'
