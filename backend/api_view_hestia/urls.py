28136e86f02438296b476dc97e75dc5e98ed468f
Verónica <vero7nicaxs@gmail.com>
2026-02-28 21:40:59 +0100

    Actualización fichero "urls.py"

--- backend/api_view_hestia/urls.py
+++ backend/api_view_hestia/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    # Agrega tus rutas aquí
-]
+# ROUTER VIEWSET
+router = DefaultRouter() 
+
+urlpatterns = router.urls
+# urlpatterns = [
+#    # Agrega tus rutas aquí
+# ]
+
 
 
