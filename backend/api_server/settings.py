d2238c419b9a1f1e87eaf9675bfffd82e69bdfbb
javier.santos <javier.santos@DESKTOP-FTRKGDP>
2026-03-03 11:06:17 -0600

    Modulo de Salas

--- backend/api_server/settings.py
+++ backend/api_server/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api_view_hestia',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS Configuration
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
--- backend/api_view_hestia/views.py
+++ backend/api_view_hestia/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from .models import Habitacion, TipoHabitacion, Sala, TipoSala, Cliente
 from .serializer import HabitacionSerializer, TipoHabitacionSerializer, SalaSerializer, TipoSalaSerializer, ClienteSerializer
 from rest_framework.response import Response
@@ -10,27 +10,12 @@ from rest_framework import status
 
 
 #----------- SALA API VIEWS -----------
-class TipoSalaList(APIView):
-    def get(self, request):
-        tipos_sala = TipoSala.objects.all()
-        if not tipos_sala.exists():
-            return Response(
-                {"message": "No hay tipos de sala disponibles en este momento"}, 
-                status=status.HTTP_404_NOT_FOUND
-            )
-        serializer = TipoSalaSerializer(tipos_sala, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
+class SalaViewSet(viewsets.ModelViewSet):
+    queryset = Sala.objects.all()
+    serializer_class = SalaSerializer
+    lookup_field = 'id'
 
-class SalaList(APIView):
-    def get(self, request):
-        salas = Sala.objects.filter(estado='disponible')
-        
-        if not salas.exists():
-            return Response(
-                {"message": "No hay salas disponibles en este momento"}, 
-                status=status.HTTP_404_NOT_FOUND
-            )
-        
-        serializer = SalaSerializer(salas, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class TipoSalaViewSet(viewsets.ModelViewSet):
+    queryset = TipoSala.objects.all()
+    serializer_class = TipoSalaSerializer
+    lookup_field = 'id'
