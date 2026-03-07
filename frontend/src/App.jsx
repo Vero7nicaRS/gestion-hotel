5683cfb7cbbf1556e3a7d90b190d555a3ca65d6f
Verónica <vero7nicaxs@gmail.com>
2026-03-07 17:29:52 +0100

    Se actualiza App agregar rutas, cabecera y pie de página. También, se modifica CSS para colocar el NavBar al principio de la página.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HabitacionDetalle from "./pages/HabitacionDetalle";
+import NavBar from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer';
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      {/* NAVBAR */}
+      <NavBar> </NavBar>
+
+      {/* RUTAS */}
+      <Routes>
+
+        <Route path="/" element={<Navigate to="/habitacion/1" replace />} />  
+        <Route path="/habitacion/:id" element={<HabitacionDetalle />} />
+        <Route path="/navbar" element={<NavBar />} />
+        <Route path='*' element={<p>404: Página no encontrada</p>} />
+      </Routes>
+
+      {/* FOOTER */}
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
